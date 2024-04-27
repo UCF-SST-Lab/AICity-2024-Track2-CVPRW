a2f225b56d0c3ec6b3ab31cc265803816bb15608
Daniel (Dongdong) Wang <50807233+dwang181@users.noreply.github.com>
2024-04-27 18:50:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ python train.py --cfg_path cfgs/bdd_ped_clip_pdvcl.yml --gpu_id=0 --no_self_iou
 #python train.py --cfg_path cfgs/bdd_veh_clip_pdvcl.yml --gpu_id=1 --no_self_iou
 
 #################################### Finetuning with WTS
-#python train.py --cfg_path cfgs/train_wts_veh_event_pdvcl_finetune.yml --gpu_id=2 --no_self_iou --load=save/bdd_veh_clip_pdvcl_v_2024-03-22-13-59-57/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
-#python train.py --cfg_path cfgs/train_wts_veh_normal_pdvcl_finetune.yml --gpu_id=3 --no_self_iou --load=save/bdd_veh_clip_pdvcl_v_2024-03-22-13-59-57/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
+#python train.py --cfg_path cfgs/train_wts_veh_event_pdvcl_finetune.yml --gpu_id=2 --no_self_iou --load=save/bdd_veh_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
+#python train.py --cfg_path cfgs/train_wts_veh_normal_pdvcl_finetune.yml --gpu_id=3 --no_self_iou --load=save/bdd_veh_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
 
-#python train.py --cfg_path cfgs/train_wts_ped_event_pdvcl_finetune.yml --gpu_id=4 --no_self_iou --load=save/bdd_ped_clip_pdvcl_v_2024-03-22-13-42-54/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
-#python train.py --cfg_path cfgs/train_wts_ped_normal_pdvcl_finetune.yml --gpu_id=5 --no_self_iou --load=save/bdd_ped_clip_pdvcl_v_2024-03-22-13-42-54/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
+#python train.py --cfg_path cfgs/train_wts_ped_event_pdvcl_finetune.yml --gpu_id=4 --no_self_iou --load=save/bdd_ped_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
+#python train.py --cfg_path cfgs/train_wts_ped_normal_pdvcl_finetune.yml --gpu_id=5 --no_self_iou --load=save/bdd_ped_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
 
 
 
