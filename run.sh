f173862f2e797fec1c7436ec40d8312a8f7e0c68
Daniel (Dongdong) Wang <50807233+dwang181@users.noreply.github.com>
2024-04-27 20:50:36 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
-#################################### Training/Pretraining with BDD
+#################################### Training with BDD #################################### 
 
 python train.py --cfg_path cfgs/bdd_ped_clip_pdvcl.yml --gpu_id=0 --no_self_iou
 #python train.py --cfg_path cfgs/bdd_veh_clip_pdvcl.yml --gpu_id=1 --no_self_iou
 
-#################################### Finetuning with WTS
-####################### specify the model load path with saved checkpointpoint folder
-#python train.py --cfg_path cfgs/train_wts_veh_event_pdvcl_finetune.yml --gpu_id=2 --no_self_iou --load=save/bdd_veh_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
-#python train.py --cfg_path cfgs/train_wts_veh_normal_pdvcl_finetune.yml --gpu_id=3 --no_self_iou --load=save/bdd_veh_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
 
-#python train.py --cfg_path cfgs/train_wts_ped_event_pdvcl_finetune.yml --gpu_id=4 --no_self_iou --load=save/bdd_ped_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
-#python train.py --cfg_path cfgs/train_wts_ped_normal_pdvcl_finetune.yml --gpu_id=5 --no_self_iou --load=save/bdd_ped_clip_pdvcl/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
+#################################### Pretraining with BDD #################################### 
+
+python train.py --cfg_path cfgs/train_bdd_ped_clip_pdvcl_pretrain.yml --gpu_id=0 --no_self_iou
+#python train.py --cfg_path cfgs/train_bdd_veh_clip_pdvcl_pretrain.yml --gpu_id=1 --no_self_iou
+
+#################################### Finetuning with WTS #################################### 
+####################### specify the model load path with saved checkpointpoint folder
+#python train.py --cfg_path cfgs/train_wts_ped_event_pdvcl_finetune.yml --gpu_id=0 --no_self_iou --load=save/bdd_ped_clip_pdvcl_pretrain/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
+#python train.py --cfg_path cfgs/train_wts_ped_normal_pdvcl_finetune.yml --gpu_id=1 --no_self_iou --load=save/bdd_ped_clip_pdvcl_pretrain/ --load_vocab data/vocabulary/vocabulary_bdd_pedestrian.json
+
+#python train.py --cfg_path cfgs/train_wts_veh_event_pdvcl_finetune.yml --gpu_id=0 --no_self_iou --load=save/bdd_veh_clip_pdvcl_pretrain/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
+#python train.py --cfg_path cfgs/train_wts_veh_normal_pdvcl_finetune.yml --gpu_id=1 --no_self_iou --load=save/bdd_veh_clip_pdvcl_pretrain/ --load_vocab data/vocabulary/vocabulary_bdd_vehicle.json
 
 
 
